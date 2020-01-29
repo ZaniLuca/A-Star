30a7adcdf4df395422ea11079ed9917372726746
Luca <lucazani2002@gmail.com>
2020-01-29 23:02:58 +0100

    - addNeighbors function - Start evaluating neighbors - Added G counter

--- A_Star.py
+++ A_Star.py
@@ -29,15 +29,58 @@ class Game:
         :return: None
         """
         clock = pygame.time.Clock()
+
         self.createGrid()
+        self.start = self.grid[0][0]
+        self.end = self.grid[self.width // self.w - 1][self.height // self.w - 1]
+        self.openSet.append(self.start)
+
         run = True
         while run:
             clock.tick(self.fps)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-            if not self.done:
-                pass
+
+            if len(self.openSet) > 0:
+                best = 0  # Index of the lowest F cell
+                for i in range(len(self.openSet)):
+                    if self.openSet[i].f < self.openSet[best].f:
+                        best = i
+
+                current = self.openSet[best]
+
+                # Win Condition
+                if self.openSet[best] == self.end:
+                    self.done = True
+                    print('Done!')
+
+                self.openSet.remove(current)  # Potrebbe non andare
+                self.closedSet.append(current)
+
+                # Evaluating neighbors
+                for i in range(len(current.neighbors)):
+                    neighbor = current.neighbors[i]
+
+                    if neighbor not in self.closedSet:
+                        temp_g = current.g + 1
+
+                        # Check if i have evaluated the neighbor before
+                        # if so we have a better G score
+                        if neighbor in self.openSet:
+                            if temp_g < neighbor.g:
+                                neighbor.g = temp_g
+                        # otherwise just give the neighbor the temp_g
+                        else:
+                            neighbor.g = temp_g
+                            self.openSet.append(neighbor)
+
+                        neighbor.h = self.heuristic(neighbor)
+
+            else:
+                print('No Solution')
+
             self.update()
 
         pygame.quit()
@@ -45,11 +88,18 @@ class Game:
     def createGrid(self):
         """
         create the cells
+        add the neighbors
         :return: None
         """
+        # Create cells
         for i in range(self.width // self.w):  # Cols
             for j in range(self.height // self.w):  # Rows
                 self.grid[i][j] = Cell(i, j)
+
+        # Adds Neighbors
+        for i in range(self.width // self.w):
+            for j in range(self.height // self.w):
+                self.grid[i][j].addNeighbors(self.grid, self.width // self.w, self.height // self.w)
 
     def update(self):
         """
@@ -57,11 +107,22 @@ class Game:
         draw the cells
         :return: None
         """
-        self.screen.fill(white)
+        self.screen.fill(black)
+
+        # DrawLoop
         for i in range(self.width // self.w):  # Cols
             for j in range(self.height // self.w):  # Rows
-                self.grid[i][j].show(self.screen, self.w)
+                self.grid[i][j].show(self.screen, self.w, white)
+
+        for i in range(len(self.openSet)):
+            self.openSet[i].show(self.screen, self.w, green)
+        for i in range(len(self.closedSet)):
+            self.closedSet[i].show(self.screen, self.w, red)
+
         pygame.display.flip()
+
+    def heuristic(self, neighbor):
+        pass
 
 
 g = Game()
--- Cell.py
+++ Cell.py
@@ -12,15 +12,32 @@ class Cell:
         """
         self.i = i
         self.j = j
+        self.f = 0
+        self.h = 0
+        self.g = 0
+        self.neighbors = []
 
-    def show(self, screen, w):
+    def show(self, screen, w, color):
         """
         Draw a rectangle
         :param screen: pygame window
         :param w: int
+        :param color: color
         :return: None
         """
-        x = self.i * w
+        x = self.i * w - 1  # -1 is for centering correcting the cells
         y = self.j * w
         rect = pygame.Rect(x, y, w, w)
-        pygame.draw.rect(screen, white, rect)
+        pygame.draw.rect(screen, color, rect)
+        pygame.draw.rect(screen, black, rect, 1)  # Border
+
+    def addNeighbors(self, grid, cols, rows):
+
+        if self.i < cols - 1:
+            self.neighbors.append(grid[self.i + 1][self.j])  # Top
+        if self.j < rows - 1:
+            self.neighbors.append(grid[self.i][self.j + 1])  # Right
+        if self.i > 0:
+            self.neighbors.append(grid[self.i - 1][self.j])  # Bottom
+        if self.j > 0:
+            self.neighbors.append(grid[self.i][self.j - 1])  # Left
